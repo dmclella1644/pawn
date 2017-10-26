1b7b19019d18e3d1f802ccc36cbce47805f2501b
dmclella1644 <31871226+dmclella1644@users.noreply.github.com>
2017-10-26 15:59:03 -0700

    Add files via upload
    added comments the MainActivity.java
    uploading comments to the xml files in a bit.

--- Pawn/app/src/main/java/com/example/pawn/pawn/MainActivity.java
+++ Pawn/app/src/main/java/com/example/pawn/pawn/MainActivity.java
@@ -23,6 +23,11 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+     * This is what happens when the activity is created. Currently, it is a
+     * blank slate that has a button on it. This can be changed during the
+     * UI development.
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,11 +40,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    /*
+     * Global variables here. mCurrentPhotoPath is a String that contains the file
+     * path to the photo. This allows the programmer to easily find out where the
+     * photo is if they want to edit it or delete it. The boolean closed is used
+     * so the crop tool is only called once. This can be changed for later. The
+     * UriForPhoto is a Uri for the photo. This is needed for the Crop command.
+     */
     String mCurrentPhotoPath;
     Uri UriForPhoto;
     boolean closed = false;
 
     public void startCropImageActivity(Uri imageURI) {
+        /*
+         * This is where the cropping, Python code, and deletion occur. Currently,
+         * the execution of the Python code is failing. Nothing wrong with Python,
+         * just Runtime.exec
+         */
         CropImage.activity(imageURI).start(this);
         //after image has been edited to have the chessboard within it, we call python code.
         /*try {
@@ -58,6 +76,12 @@ public class MainActivity extends AppCompatActivity {
         }*/
     }
 
+    /*
+     * The occurs when the Crop command is finished. This changes the photo
+     * into the cropped image. For changing the code so the user would pick
+     * 4 points, you would need to change the previous command and this
+     * command.
+     */
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
@@ -70,6 +94,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
+     * FileObserver is an abstract class that can be used by defining
+     * another class, in this case fileObserver (confusing...). FileObserver
+     * allows the program to detect if a file has been accessed, modified,
+     * written to, opened, closed, or deleted. This is possibly the easiest
+     * way to find out when a photo is taken and edit it.
+     */
     public class fileObserver extends FileObserver {
         public String absolutePath;
         public fileObserver(String path) {
@@ -111,6 +142,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    /*
+     * createImageFile creates the file name for the photo and the path to the file.
+     * The most important parts of this are the timestamp, the file path, and the
+     * file directory. The timestamp is the name for the file, so there should not be
+     * any overlap between different photos taken by the same phone. Another way
+     * of doing this is to put a counter at the end of the image name. The file path
+     * is required for other parts of the program, which is why mCurrentPhotoPath is
+     * a global variable. The file directory is a private directory, which means no
+     * other apps or phones can access this directory. This is not required, but if
+     * the plan is to delete the photos after creating them and converting them, then
+     * this allows other programs to not access the photos and for the user to not
+     * see them. However, we may want to change it, if the users want to see the photos.
+     * If so, change getExternalFilesDir to getExternalStoragePublicDirectory for the
+     * storageDir variable. This makes the directory public, so other apps and phones
+     * can access them.
+     */
     private File createImageFile() throws IOException {
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp;
@@ -121,6 +168,15 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
+    /*
+     * dispatchTakePictureIntent tells the phone to use its camera app to take
+     * a photo. It also calls code for the file name and path and creates a fileObserver
+     * for the file path. Currently, the fileObserver does not "stopWatching" for changes
+     * to the file until it goes out of scope. This is due to calling test.stopWatching()
+     * before going out of scope ends up with the crop command not being called. Also, the
+     * event 32768 gets called and I'm not sure what that does, if test.stopWatching() is put
+     * after startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO).
+     */
     static final int REQUEST_TAKE_PHOTO = 1;
     private void dispatchTakePictureIntent(){
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
