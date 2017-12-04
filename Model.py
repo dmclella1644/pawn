5c88233489540be154416673b10f79d90e95414e
David <dmclella@ucsc.edu>
2017-12-04 01:13:28 -0800

    uploading all files

---
+++ Model.py
@@ -0,0 +1,103 @@
+# Copyright 2017 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import sys
+
+import numpy as np
+import tensorflow as tf
+
+class Model:
+    def __init__(self):
+    # Fix the file sources later
+        self.model_file = 'pawn_graph.pb'
+        self.label_file = 'pawn_labels.txt'
+        self.graph = self.load_graph()
+        self.labels = self.load_labels()
+        
+    def load_graph(self):
+        self.graph = tf.Graph()
+        graph_def = tf.GraphDef()
+
+        with open(self.model_file, "rb") as f:
+            graph_def.ParseFromString(f.read())
+        with self.graph.as_default():
+            tf.import_graph_def(graph_def)
+
+        return self.graph
+
+    def read_tensor_from_image_file(self, file_name, input_height=299, input_width=299,
+                    input_mean=0, input_std=255):
+        input_name = "file_reader"
+        output_name = "normalized"
+        file_reader = tf.read_file(file_name, input_name)
+        if file_name.endswith(".png"):
+            image_reader = tf.image.decode_png(file_reader, channels = 3,
+                                           name='png_reader')
+        elif file_name.endswith(".gif"):
+            image_reader = tf.squeeze(tf.image.decode_gif(file_reader,
+                                                      name='gif_reader'))
+        elif file_name.endswith(".bmp"):
+            image_reader = tf.image.decode_bmp(file_reader, name='bmp_reader')
+        else:
+            image_reader = tf.image.decode_jpeg(file_reader, channels = 3,
+                                            name='jpeg_reader')
+        float_caster = tf.cast(image_reader, tf.float32)
+        dims_expander = tf.expand_dims(float_caster, 0);
+        resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
+        normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
+        sess = tf.Session()
+        result = sess.run(normalized)
+
+        return result
+
+    def load_labels(self):
+        label = []
+        proto_as_ascii_lines = tf.gfile.GFile(self.label_file).readlines()
+        for l in proto_as_ascii_lines:
+          label.append(l.rstrip())
+        return label
+
+    def labelImage(self, image):
+        input_height = 299
+        input_width = 299
+        input_mean = 0
+        input_std = 255
+        input_layer = "Mul"
+        output_layer = "final_result"
+
+        t = self.read_tensor_from_image_file(image,
+                                      input_height=input_height,
+                                      input_width=input_width,
+                                      input_mean=input_mean,
+                                      input_std=input_std)
+
+        input_name = "import/" + input_layer
+        output_name = "import/" + output_layer
+        input_operation = self.graph.get_operation_by_name(input_name);
+        output_operation = self.graph.get_operation_by_name(output_name);
+
+        with tf.Session(graph=self.graph) as sess:
+            results = sess.run(output_operation.outputs[0],
+                          {input_operation.outputs[0]: t})
+        results = np.squeeze(results)
+
+        #Return label of the top result
+        top_k = results.argsort()[-1:][::-1]
+        return self.labels[top_k[0]]
